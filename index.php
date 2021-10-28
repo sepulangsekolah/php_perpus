23fd17096b24f845e66ceaa69d7b2d5a8515b930
Alif Fathur <herobuxx@gmail.com>
2021-10-28 09:28:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+  <head>
+    <title>Header bro?</title>
+    <!-- Import Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
+    <!-- Import Local CSS -->
+    <link rel="stylesheet" href="style.css" />
+    <!-- Import icon CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div id="header">
+        <h1>Welcome to Alif's PHP Test</h1>
+    </div>
+
+    <div id="wrap">
+        <div ="side_menu">
+            <div id="bg_menu">Menu</div>
+    </div>
+
+    <div id="sidebar">
+        <p>Hello there! Whats up?</p>
+        <br>
+        <a href="#">Log out</a>
+        <br>
+        <a href="#">conquerOS Source</a>
+        <br>
+        <a href="#">lunatic. Source</a>
+        <br>
+        <a href="#">halcyon! Source</a>
+    </div>
+
+    </div>
+
+    <div id="menu_tengah">
+        <div id="deez_nuts" style="text-align: center;">
+            <marquee>Sometimes, we have to chill</marquee>
+        </div>
+    <div id="hero_isi">
+    </div>
+    </div>
+
+    <div id="footer">
+        Web ini milik Alif Fathur, kek
+</div>
+</body>
+</html>
